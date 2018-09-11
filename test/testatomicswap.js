2748d3e44ed7a5cca26fdd2d907cffa1095569c6
block <rm644111@gmail.com>
2018-09-11 07:19:08 -0700

    ether

---
+++ test/testatomicswap.js
@@ -0,0 +1,27 @@
+
+var AbiConfig = require("../abi/atomicswap");
+var AppConfig = require("../config");
+var AtomicSwap = require("../modules/atomicswap");
+var Common = require("../modules/common");
+
+var secret = (new Common()).GenerateSecret();
+
+// Init
+var atomicSwap = new AtomicSwap(AbiConfig, AppConfig.hosts[0]);
+try {
+//	var temp;
+    atomicSwap.Initiate(7200, "0x" + secret.hashedSecret, AppConfig.hosts[1].defaultWallet, 10, 2000000).then(function (result) {
+//    	var temp = result;
+        console.log("Generated initial transaction: " + result);
+    });
+//    console.log("Generated initial transaction: " + temp);
+    atomicSwap.Participate(7200, "0x" + secret.hashedSecret, AppConfig.hosts[1].defaultWallet, 10, 2000000).then(function (result) {
+        console.log("Participated with transaction number: " + result);
+    }).catch(function(err) {
+        console.log(err);
+    });
+} catch (e) {
+    console.log(e);
+
+}
+
